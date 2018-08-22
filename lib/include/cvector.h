c213b02f2fd9975ec2a97f284b47a324d8b5cb55
Marco Krahl <marco-krahl@web.de>
2018-08-22 22:58:13 +0200

    * Moved implementation of CVector functions from .cpp to .h due to compiler errors * Deleted cvector.cpp due to implementation in .h file

--- lib/include/cvector.h
+++ lib/include/cvector.h
@@ -49,7 +49,11 @@ public:
   // ================================================
 
   // Default Constructor
-  CVector(size_t dim);
+  CVector(size_t dim) : m_dim(dim)
+  {
+      m_ptVector = malloc(dim * sizeof(T));
+      m_type = EVectorTypes::None;
+  }
 
   /* TO DO
   // Constructor for special type Vector
@@ -57,7 +61,10 @@ public:
   */
 
   // Constructor to initialize the Vector directly
-  CVector(T &ptVector, size_t dim);
+  CVector(T &ptVector, size_t dim) : m_dim(dim)
+  {
+      m_ptVector = ptVector;
+  }
 
   // Copy Constructor
   CVector(const CVector<T> &vec);
@@ -69,26 +76,161 @@ public:
   // [PUBLIC] OPERATORS
   // ================================================
 
-  CVector<T> &operator=(const CVector<T> &vec); // copy assignment operator
+  // copy assignment operator
+  CVector<T> &operator=(const CVector<T> &vec)
+  {
+      if(this != &vec)
+      {
+          delete [] m_ptVector;
+          copy(vec);
+      }
 
-  T &operator()(size_t idx);
-  T operator()(size_t idx) const;
+      return *this;
+  }
 
-  bool operator==(const CVector<T> &vec) const;
-  bool operator!=(const CVector<T> &vec) const;
+  T &operator()(size_t idx)
+  {
+      if (idx > m_dim)
+          throw std::out_of_range("Index out of range.");
 
-  CVector<T> operator+(const CVector<T> &vec);
-  CVector<T> &operator+=(const CVector<T> &vec);
+      return m_ptVector[idx];
+  }
 
-  CVector<T> operator-(const CVector<T> &vec);
-  CVector<T> &operator-=(const CVector<T> &vec);
+  T operator()(size_t idx) const
+  {
+      if (idx > m_dim)
+          throw std::out_of_range("Index out of range.");
 
-  T operator*(const CVector<T> &vec);   // scalar / dot product
+      return m_ptVector[idx];
+  }
 
-  CVector<T> &operator*(const T &vec); // scaling
+  bool operator==(const CVector<T> &vec) const
+  {
+      if ((m_dim != vec.m_dim))
+      {
+          return false;
+      }
 
-  CVector<T> operator^(const CVector<T> &vec);   // cross product
-  CVector<T> &operator^=(const CVector<T> &vec); // cross product
+      for (size_t i = 0; i < m_dim; i++)
+      {
+          if (m_ptVector[i] != vec.m_ptVector[i])
+          {
+              return false;
+          }
+      }
+
+      return true;
+  }
+
+  bool operator!=(const CVector<T> &vec) const
+  {
+      return !((*this) == vec);
+  }
+
+  CVector<T> operator+(const CVector<T> &vec)
+  {
+      if (m_dim != vec.m_dim)
+          throw std::range_error("Dimensions must be the same");
+
+      CVector<T> tmp(*this);
+
+      for(size_t i = 0; i < m_dim; i++)
+      {
+          tmp.m_ptVector[i] += vec.m_ptVector[i];
+      }
+
+      return tmp;
+  }
+
+  CVector<T> &operator+=(const CVector<T> &vec)
+  {
+      if (m_dim != vec.m_dim)
+          throw std::range_error("Dimensions must be the same");
+      
+      for(size_t i = 0; i < m_dim; i++)
+      {
+          m_ptVector[i] += vec.m_ptVector[i];
+      }
+
+      return *this;
+  }
+
+  CVector<T> operator-(const CVector<T> &vec)
+  {
+      if (m_dim != vec.m_dim)
+          throw std::range_error("Dimensions must be the same");
+
+      CVector<T> tmp(*this);
+
+      for(size_t i = 0; i < m_dim; i++)
+      {
+          tmp.m_ptVector[i] -= vec.m_ptVector[i];
+      }
+
+      return tmp;
+  }
+
+  CVector<T> &operator-=(const CVector<T> &vec)
+  {
+      if (m_dim != vec.m_dim)
+          throw std::range_error("Dimensions must be the same");
+      
+      for(size_t i = 0; i < m_dim; i++)
+      {
+          m_ptVector[i] -= vec.m_ptVector[i];
+      }
+
+      return *this;
+  }
+
+  // scalar / dot product
+  T operator*(const CVector<T> &vec)
+  {
+      if (m_dim != vec.m_dim)
+          throw std::range_error("Dimensions must be the same");
+      
+      T product(0.0);
+
+      for (size_t i = 0; i < m_dim; i++)
+          product += m_ptVector[i] * vec.m_ptVector[i];
+
+      return product;
+  }
+
+  // scaling
+  CVector<T> &operator*(const T &vec)
+  {
+      for (size_t i = 0; i < m_dim; i++)
+          m_ptVector[i] = vec * m_ptVector[i];
+
+      return *this;
+  }
+
+  CVector<T> operator^(const CVector<T> &vec)
+  {
+      if (m_dim != 3 || vec.m_dim != 3)
+          throw std::range_error("Only defined for 3D vectors");
+      
+      CVector<T> tmp(*this);
+
+      tmp.m_ptVector[0] = tmp.m_ptVector[2]*vec.m_ptVector[3] - tmp.vec.m_ptVector[3]*m_ptVector[2];
+      tmp.m_ptVector[1] = tmp.m_ptVector[3]*vec.m_ptVector[1] - tmp.vec.m_ptVector[1]*m_ptVector[3];
+      tmp.m_ptVector[2] = tmp.m_ptVector[1]*vec.m_ptVector[2] - tmp.vec.m_ptVector[2]*m_ptVector[1];
+
+      return tmp;
+  }
+
+  CVector<T> &operator^=(const CVector<T> &vec)
+  {
+      if (m_dim != 3 || vec.m_dim != 3)
+          throw std::range_error("Only defined for 3D vectors");
+      
+      m_ptVector[0] = m_ptVector[2]*vec.m_ptVector[3] - vec.m_ptVector[3]*m_ptVector[2];
+      m_ptVector[1] = m_ptVector[3]*vec.m_ptVector[1] - vec.m_ptVector[1]*m_ptVector[3];
+      m_ptVector[2] = m_ptVector[1]*vec.m_ptVector[2] - vec.m_ptVector[2]*m_ptVector[1];
+
+      return *this;
+  }
 
   // ================================================
   // [PUBLIC] FUNCTIONS
@@ -98,8 +240,22 @@ public:
 
   EVectorTypes getType() const { return m_type; }
 
-  void show();
-  std::string toString();
+  void show()
+  {
+    std::cout << this->toString() << std::endl;
+  }
+
+  std::string toString()
+  {
+    std::string str = "[";
+
+    for (size_t i = 0; i < m_dim; i++)
+        str += m_ptVector[i];
+
+    str += "]";
+
+    return str;
+  }
 
   void set_value(size_t idx, T value);
 
@@ -107,7 +263,31 @@ public:
   // [PUBLIC] MATHEMATICAL FUNCTIONS
   // ================================================
 
-  T norm(EVectorNorms norm);
+  T norm(EVectorNorms norm)
+  {
+
+    T result(0.0);
+
+    switch (norm)
+    {
+    case EVectorNorms::L1:
+        for (size_t i = 0; i < m_dim; i++)
+            result += abs(m_ptVector[i]);
+        break;
+
+    case EVectorNorms::L2:
+        for (size_t i = 0; i < m_dim; i++)
+            result += pow(m_ptVector[i], 2);
+        result = sqrt(result);
+        break;
+
+    case EVectorNorms::MaxNorm:
+        result = max(m_ptVector);
+        break;
+    }
+
+    return result;
+  }
 
   float angle_to(const CVector<T> &vec);
 

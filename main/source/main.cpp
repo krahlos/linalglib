2cb821f49a9aaaa8dbed61880e3b6e45e8cc6187
Marco Krahl <marco-krahl@web.de>
2018-08-22 20:09:47 +0200

    Commit of main.cpp with initial test of CVector class, does not work yet on macOS

--- main/source/main.cpp
+++ main/source/main.cpp
@@ -7,5 +7,11 @@ int main(int argc, const char * argv [])
 {
     std::cout << "Hello @ LinAlg C++ Project" << std::endl;
 
+    LinAlg::Vector::CVector<float> vec1(3);
+
+    vec1.show();
+
+    std::cout << "Good Bye!" << std::endl;    
+
     return 0;
 }

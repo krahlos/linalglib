c80c0c2414e1107c08d5fb29c574ad246da64c13
marco1893 <marco-krahl@web.de>
2018-08-26 20:44:02 +0200

    CVector copy constructor
    bug fixes

--- main/source/main.cpp
+++ main/source/main.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 
 #include <cmatrix.h>
-#include <cvector.h>
 
 int main(int argc, const char * argv [])
 {
     std::cout << "Hello @ LinAlg C++ Project" << std::endl;
 
-    LinAlg::Vector::CVector<float> vec1(3);
+    LinAlg::Vector::CVector<float> vec1(3), vec2(3);
 
     vec1.set_value(0, 1.0);
     vec1.set_value(1, 2.0);
     vec1.set_value(2, 3.0);
 
+    vec2.set_value(0, 1.0);
+    vec2.set_value(1, 2.0);
+    vec2.set_value(2, 3.0);
+
     vec1.show();
+    vec2.show();
+
+    LinAlg::Vector::CVector<float> vec3 = vec1 + vec2;
+
+    std::cout << "vec3: " << vec3.to_string() << std::endl;
 
     std::cout << "Good Bye!" << std::endl;    
 

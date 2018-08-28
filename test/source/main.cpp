4cc8a591378b48f59b5ee4666c0ee41fba1f49a3
marco1893 <marco-krahl@web.de>
2018-08-28 09:45:23 +0200

    GTest commissioning
    * added complete googletest code to repo
    * adaption of CMakeLists
    * adaption of main.cpp to run gtests

--- test/source/main.cpp
+++ test/source/main.cpp
@@ -1,21 +1,12 @@
 #include <iostream>
 
-#include <cmatrix.h>
-#include <cvector.h>
+#include "cmatrix.h"
+#include "cvector.h"
+#include <gtest/gtest.h>
 
-int main(int argc, const char * argv [])
-{
-    std::cout << "Hello @ LinAlg C++ Project" << std::endl;
+int main(int argc, char* argv[])
+{    
+    testing::InitGoogleTest(&argc, argv);
 
-    LinAlg::Vector::CVector<float> vec1(3);
-
-    vec1.set_value(0, 1.0);
-    vec1.set_value(1, 2.0);
-    vec1.set_value(2, 3.0);
-
-    vec1.show();
-
-    std::cout << "Good Bye!" << std::endl;    
-
-    return 0;
+    return RUN_ALL_TESTS();
 }
